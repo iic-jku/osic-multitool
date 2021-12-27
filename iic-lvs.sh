29e30f0fba7475f944496749a5e3d4b8c64fcf2c
Harald Pretl <harald@pretl.eu>
2021-12-28 00:38:25 +0100

    LVS now also works with powered Verilog .v files.

--- iic-lvs.sh
+++ iic-lvs.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
-# ==============================================================
+# ==================================================================
 # SKY130 LVS (Layout-vs-Schematic) Check
 #
 # (c) 2021 Harald Pretl
-# Institute for Integrated Circuits
-# Johannes Kepler University Linz
+# Institute for Integrated Circuits, Johannes Kepler University Linz
 #
-# Usage: iic-osic <cellname>
+# Usage: iic-lvs <cellname>
 #
 # The script expects the schematic <cellname>.sch in the current
 # directory or in subfolder `sch`. The layout <cellname>.mag
 # has to be located in the current directory or in the subfolder
 # `lay`.
-# ==============================================================
+#
+# The script can also compare a powered Verilog netlist named
+# <cellname.v> to a layout.
+# ==================================================================
 
-export ERR_LVS_MISMATCH=1
-export ERR_FILE_NOT_FOUND=2
-export ERR_NO_PARAM=3
+ERR_LVS_MISMATCH=1
+ERR_FILE_NOT_FOUND=2
+ERR_NO_PARAM=3
 
 if [ $# != 1 ]; then
 	echo "Usage: $0 <cellname>"
@@ -25,23 +27,29 @@ fi
 
 # Define useful variables
 # -----------------------
-export CELL_SCH="$1.sch"
-export CELL_LAY="$1.mag"
-export EXT_SCRIPT="ext_$1.tcl"
-export NETLIST_SCH="$1.sch.sp"
-export NETLIST_LAY="$1.ext.sp"
-export LVS_REPORT="$1.lvs.out"
-export LVS_LOG="$1.lvs.log"
-export TOPCELL="$1"
+CELL_SCH="$1.sch"
+CELL_V="$1.v"
+CELL_LAY="$1.mag"
+EXT_SCRIPT="ext_$1.tcl"
+NETLIST_SCH="$1.sch.sp"
+NETLIST_LAY="$1.ext.sp"
+LVS_REPORT="$1.lvs.out"
+LVS_LOG="$1.lvs.log"
+TOPCELL="$1"
 
 # Check if files exist
 # --------------------
-if [ ! -f "$CELL_SCH" ]; then
-	if [ ! -f "sch/$CELL_SCH" ]; then
-		echo "Schematic $CELL_SCH not found!"
-		exit $ERR_FILE_NOT_FOUND
-	else
-		export CELL_SCH="sch/$CELL_SCH"
+if [ -f "$CELL_V" ]; then
+	VERILOG_MODE=1
+else
+	VERILOG_MODE=0
+	if [ ! -f "$CELL_SCH" ]; then
+		if [ ! -f "sch/$CELL_SCH" ]; then
+			echo "Schematic $CELL_SCH not found!"
+			exit $ERR_FILE_NOT_FOUND
+		else
+			export CELL_SCH="sch/$CELL_SCH"
+		fi
 	fi
 fi
 
@@ -56,11 +64,17 @@ fi
 
 # Initial checks passed, start working
 # ------------------------------------
-echo "Running LVS of <$CELL_SCH> vs <$CELL_LAY>."
+if [ $VERILOG_MODE -eq 0 ]; then
+	echo "Running LVS of <$CELL_LAY> vs <$CELL_SCH>."
+else
+	echo "Running LVS of <$CELL_LAY> vs <$CELL_V>."
+fi
 
 # Extract SPICE netlist from schematic
 # ------------------------------------
-xschem -n -s -q --no_x --tcl 'set top_subckt 1' "$CELL_SCH" -N "$NETLIST_SCH"
+if [ $VERILOG_MODE -eq 0 ]; then
+	xschem -n -s -q --no_x --tcl 'set top_subckt 1' "$CELL_SCH" -N "$NETLIST_SCH"
+fi
 
 # Generate extract script for magic
 # ---------------------------------
@@ -76,8 +90,16 @@ magic -dnull -noconsole "$EXT_SCRIPT" > /dev/null
 
 # Now run the lvs using netgen
 # ----------------------------
-netgen -batch lvs "$NETLIST_SCH $TOPCELL" "$NETLIST_LAY $TOPCELL" $PDK_ROOT/sky130A/libs.tech/netgen/sky130A_setup.tcl \
-	"$LVS_REPORT" > "$LVS_LOG"
+if [ $VERILOG_MODE -eq 0 ]; then
+	netgen -batch lvs "$NETLIST_LAY $TOPCELL" "$NETLIST_SCH $TOPCELL" \
+		$PDK_ROOT/sky130A/libs.tech/netgen/sky130A_setup.tcl \
+		"$LVS_REPORT" > "$LVS_LOG"
+else
+	export MAGIC_EXT_USE_GDS=1
+	netgen -batch lvs "$NETLIST_LAY $TOPCELL" "$CELL_V $TOPCELL" \
+                $PDK_ROOT/sky130A/libs.tech/netgen/sky130A_setup.tcl \
+                "$LVS_REPORT" > "$LVS_LOG"
+fi
 
 # Finished
 # --------
