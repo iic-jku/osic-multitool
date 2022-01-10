130417cb22b500f804c7699fa272b95088ccb940
Harald Pretl <harald.pretl@jku.at>
2022-01-10 10:54:32 +0100

    Clean now hierarchically.

--- iic-clean.sh
+++ iic-clean.sh
@@ -7,15 +7,15 @@
 # Johannes Kepler University Linz
 # ==============================================================
 
-echo "Cleaning up files."
+echo "Cleaning up files (hierarchically)."
 
-rm -f *.spc
-rm -f *.ext
-rm -f *.log
-rm -f *.out
-rm -f *.raw
-rm -f *.spice
-rm -f *.vcd
-rm -f ext*.tcl
-rm -f drc*.tcl
-rm -f pex*.tcl
+rm -rf *.spc
+rm -rf *.ext
+rm -rf *.log
+rm -rf *.out
+rm -rf *.raw
+rm -rf *.spice
+rm -rf *.vcd
+rm -rf ext*.tcl
+rm -rf drc*.tcl
+rm -rf pex*.tcl
