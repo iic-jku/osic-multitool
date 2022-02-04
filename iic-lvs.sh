61c2617fc97b2db32c763568a8659729643d8963
Harald Pretl <harald@pretl.eu>
2022-02-04 16:32:17 +0100

    Added license headers to files.

--- iic-lvs.sh
+++ iic-lvs.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
-# ==================================================================
+# ========================================================================
 # SKY130 LVS (Layout-vs-Schematic) Check
 #
-# (c) 2021-2022 Harald Pretl
-# Institute for Integrated Circuits, Johannes Kepler University Linz
+# SPDX-FileCopyrightText: 2021-2022 Harald Pretl, Johannes Kepler 
+# University, Institute for Integrated Circuits
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 # Usage: iic-lvs <cellname>
 #
@@ -17,7 +30,7 @@
 #
 # The LVS script als can compare a netlist created from a 
 # Powered-Verilog-to-xschem-schematic conversion using iic-v2sch.
-# ==================================================================
+# ========================================================================
 
 # ERR_LVS_MISMATCH=1 reserved
 ERR_FILE_NOT_FOUND=2
--- iic-spice-model-red.py
+++ iic-spice-model-red.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
+# ========================================================================
+# SPDX-FileCopyrightText: 2021-2022 Harald Pretl, Johannes Kepler 
+# University, Institute for Integrated Circuits
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 # This script traverses SPICE model files (e.g. from SKY130) and
 # extracts only the wanted model section, removes all comments and
 # empty lines, and resolves all includes so that a flat model file
 # results. This should speed up ngspice starts.
-#
-# (c) 2021 Harald Pretl, Johannes Kepler University Linz
+# ========================================================================
 
 import sys,re,os
 
