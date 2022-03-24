ca380f7e675090406e99e76fd1e6dea22044476f
Harald Pretl <harald@pretl.eu>
2022-03-24 16:29:20 +0100

    Changed location of PDKs (open_pdks is failing otherwise).

--- iic-osic-setup.sh
+++ iic-osic-setup.sh
@@ -24,7 +24,7 @@
 
 # Define setup environment
 # ------------------------
-export MY_PDK=/usr/local/share/pdk
+export MY_PDK="$HOME/pdk"
 export MY_STDCELL=sky130_fd_sc_hd
 export SRC_DIR="$HOME/src"
 export OPENLANE_DIR="$HOME/OpenLane"
