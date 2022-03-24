0e8d7ecffc032e3b2194d7065ab89870aa353172
Harald Pretl <harald@pretl.eu>
2022-03-24 16:30:01 +0100

    Caravel setup is WIP.

--- caravel_setup/iic-init-caravel.sh
+++ caravel_setup/iic-init-caravel.sh
@@ -12,5 +12,7 @@ export CARAVEL_ENV_ROOT="$HOME/caravel_mpw5"
 export PDK_ROOT="$CARAVEL_ENV_ROOT/pdks"
 export OPENLANE_ROOT="$CARAVEL_ENV_ROOT/openlane"
 export PRECHECK_ROOT="$CARAVEL_ENV_ROOT/precheck"
+export OPEN_PDKS_COMMIT=c5730b574461889c82858b08d12ba42423d9c2cb
+export CARAVEL_ROOT="$CARAVEL_ENV_ROOT/iic-audiodac-v1/caravel"
 #
 export STD_CELL_LIBRARY=sky130_fd_sc_hd
