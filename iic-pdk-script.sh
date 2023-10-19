bbbcbab19aa250d1574d0f1f31fe82cbd146a598
Harald Pretl <harald.pretl@jku.at>
2023-10-19 10:14:16 +0200

    Script for fast PDK switching (needs to be sourced, so setup alias)

---
+++ iic-pdk-script.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+# ========================================================================
+# Switch PDKs (for IIC-OSIC-TOOLS)
+#
+# SPDX-FileCopyrightText: 2023 Harald Pretl
+# Johannes Kepler University, Institute for Integrated Circuits
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Usage: iic-pdk.sh <pdk>
+#
+# ========================================================================
+
+ERROR=0
+
+# Print out usage
+# ---------------
+
+if [ $# = 0 ]; then
+
+	echo
+	echo "Switch PDKs"
+	echo
+	echo "Usage: iic-pdk <pdk> [<stdcell-lib>]"
+	echo
+	if [ -d "$PDK_ROOT" ]; then
+		echo "Available PDKs:"
+		# shellcheck disable=SC2010
+		ls "$PDK_ROOT" | grep -v volare
+		echo
+	fi
+
+else
+
+	# check if PDK_ROOT is set, if not, set it to the default location 
+	if [ -z ${PDK_ROOT+z} ]; then
+		if [ -d /foss/pdks ]; then
+			export PDK_ROOT /foss/pdks
+		else
+			echo "[ERROR] Variable PDK_ROOT is not set, and default location (/foss/pdks) not found!"
+			ERROR=1
+		fi
+	fi
+
+	# set PDK variables
+	if [ -d "$PDK_ROOT/$1" ]; then
+		export PDK="$1"
+		export PDKPATH="$PDK_ROOT/$PDK"
+	else
+		echo "[ERROR] PDK directory $PDK_ROOT/$1 not found!"
+		ERROR=1
+	fi
+
+	if [ $ERROR = 0 ]; then
+		echo "PDK_ROOT=$PDK_ROOT"
+		echo "PDK=$PDK"
+		echo "PDKPATH=$PDKPATH"
+		#echo
+		#echo "[DONE] Bye!"
+	fi
+
+fi
